22f9b4be5d6055b3a5595def085b544a2e93318f
Aidan <aidanream01@gmail.com>
2026-02-11 19:18:05 -0700

    ball!

--- src/vec3.h
+++ src/vec3.h
@@ -10,6 +10,7 @@ class vec3{
 
 		vec3() : e{ 0,0,0 } {}
 		vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
+		vec3(double allVal) : e{ allVal, allVal, allVal } {}
 
 		double x() const { return e[0]; }
 		double y() const { return e[1]; }
