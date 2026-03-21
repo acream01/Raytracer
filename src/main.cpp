75ad923c09c9f4b51fc8fc57e768b300f84e63ef
Aidan <aidanream01@gmail.com>
2026-03-21 16:10:10 -0600

    Added quad primitives!

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,10 @@
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
+#include "quad.h"
 #include "texture.h"
 
-//Chapter 6.0 Quadrilaterals
+//Chapter 7.0 Lights
 
 // Scenes
 void bouncing_spheres(hittable_list& world, camera& cam) {
@@ -171,15 +172,42 @@ void perlin_sphere(hittable_list& world, camera& cam) {
 
 }
 
+void quads(hittable_list& world, camera& cam) {
+   
+    // Materials
+    auto left_red = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+    auto back_green = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+    auto right_blue = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+    auto lower_teal = make_shared<lambertian>(color(0.2, 0.8, 0.8));
+
+    // Quads
+    world.add(make_shared<quad>(point3(-3, -2, 5), vec3(0, 0, -4), vec3(0, 4, 0), left_red));
+    world.add(make_shared<quad>(point3(-2, -2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+    world.add(make_shared<quad>(point3(3, -2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+    world.add(make_shared<quad>(point3(-2, -3, 5), vec3(4, 0, 0), vec3(0, 0, -4), lower_teal));
+
+    cam.aspect_ratio = 1.0;
+    cam.img_width = 1600;
+    cam.samples_per_pixel = 500;
+    cam.max_depth = 50;
+
+    cam.vfov = 80;
+    cam.lookfrom = point3(0, 0, 9);
+    cam.lookat = point3(0, 0, 0);
+    cam.up = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+}
+
+
 int main(int argc, char* argv[]) {
     hittable_list world;
     camera cam;
 
-    //bouncing_spheres(world, cam);
-    //sphere_example(world, cam);
-    //checkered_spheres(world, cam);
-    //earth(world, cam);
-    perlin_sphere(world, cam);
+    quads(world, cam);
+
 
     if (argc > 1 && check_file_extention(argv[1])) {
         cam.render(world, argv[1]);
