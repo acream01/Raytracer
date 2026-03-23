fb57d023790cc646694b2d38a206111cbb6a293f
Aidan <aidanream01@gmail.com>
2026-03-22 22:43:55 -0600

    instancing, Translation, and rotation!

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 #include "quad.h"
 #include "texture.h"
 
-//Chapter 7.4 Cornel Box
+//Chapter 9 Volumes
 
 // Scenes
 void bouncing_spheres(hittable_list& world, camera& cam) {
@@ -238,11 +238,53 @@ void simple_light(hittable_list& world, camera& cam) {
     //cam.background = color(0.70, 0.80, 1.00);
     
 }
+
+void cornell_box(hittable_list& world, camera& cam) {
+
+    auto red   = make_shared<lambertian>(color(0.65 , 0.05, 0.05));
+    auto white = make_shared<lambertian>(color(0.73, 0.73, 0.73));
+    auto green = make_shared<lambertian>(color(0.12, 0.45, 0.15));
+    auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+    world.add(make_shared<quad>(point3(555, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555), green));
+    world.add(make_shared<quad>(point3(0, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555), red));
+    world.add(make_shared<quad>(point3(343, 554, 332), vec3(-130, 0, 0), vec3(0, 0, -105), light));
+    world.add(make_shared<quad>(point3(0, 0, 0), vec3(555, 0, 0), vec3(0, 0, 555), white));
+    world.add(make_shared<quad>(point3(555, 555, 555), vec3(-555, 0, 0), vec3(0, 0, -555), white));
+    world.add(make_shared<quad>(point3(0, 0, 555), vec3(555, 0, 0), vec3(0, 555, 0), white));
+    
+    shared_ptr<hittable> box1 = box(point3(0), point3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+    world.add(box1);
+
+    shared_ptr<hittable> box2 = box(point3(0), point3(165, 165, 165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+    world.add(box2);
+
+    //Camera Settings
+    cam.aspect_ratio = 1.0;
+    cam.img_width = 600;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+    cam.background = color(0);
+
+    cam.vfov = 40;
+    cam.lookfrom = point3(278, 278, -800);
+    cam.lookat = point3(278, 278, 0);
+    cam.up = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+}
+
 int main(int argc, char* argv[]) {
     hittable_list world;
     camera cam;
 
-    simple_light(world, cam);
+    
+    cornell_box(world, cam);
 
 
     if (argc > 1 && check_file_extention(argv[1])) {
